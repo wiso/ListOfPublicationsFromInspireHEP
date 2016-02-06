2b02f2a70616998c922719fe34a765081cc770d1
Ruggero Turra <giurrero@gmail.com>
2016-02-06 01:59:36 +0100

    improving checks

--- check_biblio.py
+++ check_biblio.py
@@ -2,6 +2,12 @@ import os
 from io import open
 import subprocess
 import re
+from glob import glob
+
+regex_unicode = re.compile('[^\x00-\x7F]')
+def help_unicode(item):
+    m = regex_unicode.search(item)
+    print item[:m.start()] + "***UNICODE****" + item[m.start():m.end()] + "*****UNICODE******" + item[m.end():]
 
 
 def modify_item(item):
@@ -9,7 +15,7 @@ def modify_item(item):
     if not editor_command:
         print "you haven't defined a default EDITOR, (e.g. export EDITOR=emacs)"
         editor_command = raw_input("enter the command to open an editor (e.g. emacs/atom/...): ")
-        os.environ['editor'] = editor_command
+        os.environ['EDITOR'] = editor_command
     editor_command = editor_command.strip()
 
     with open('correction.txt', 'w') as f:
@@ -19,6 +25,9 @@ def modify_item(item):
         new_item = f.read()
     return new_item
 
+tmp_files = glob('tmp*')
+for f in tmp_files:
+    os.remove(f)
 
 f = open('bibtex.bib')
 biblio = f.read()
@@ -28,43 +37,57 @@ regex_key = re.compile(r'@[a-z]+\{([A-Za-z0-9:]+),')
 latex_template = r"""
 \documentclass{article}
 \usepackage[backend=bibtex, style=numeric-comp, sorting=none, firstinits=true, defernumbers=true]{biblatex}
-\usepackage{amsmath}
 \addbibresource{tmp.bib}
+\usepackage{amsmath}
+\usepackage[utf8]{inputenc}
+\usepackage{syntonly}
+\syntaxonly
 \begin{document}
 Try to cite: \cite{CITATION}.
 \printbibliography
 \end{document}
 """
+try:
+    substitutions = []
+    for item in biblio.split("\n\n"):
+        if '@' not in item:
+            continue
+        print item
+        tmp_biblio = open('tmp.bib', 'w')
+        tmp_biblio.write(item)
+        tmp_biblio.close()
+        m = regex_key.search(item)
+        key = m.group(1)
 
-substitutions = []
-for item in biblio.split("\n\n"):
-    if '@' not in item:
-        continue
-    print item
-    tmp_biblio = open('tmp.bib', 'w')
-    tmp_biblio.write(item)
-    tmp_biblio.close()
-    m = regex_key.search(item)
-    key = m.group(1)
+        latex_file = open('tmp.tex', 'w')
+        latex = latex_template.replace('CITATION', key)
+        latex_file.write(latex)
+        latex_file.close()
 
-    latex_file = open('tmp.tex', 'w')
-    latex = latex_template.replace('CITATION', key)
-    latex_file.write(latex)
-    latex_file.close()
-
-    error = False
-    while True:
-        try:
-            subprocess.check_call(['pdflatex', '-interaction=nonstopmode', 'tmp.tex'])
-            subprocess.check_call(['bibtex', 'tmp'])
-            subprocess.check_call(['pdflatex', '-interaction=nonstopmode', 'tmp.tex'])
-            subprocess.check_call(['pdflatex', '-interaction=nonstopmode', 'tmp.tex'])
-        except subprocess.CalledProcessError as error:
-            print "problem running %s with item %s" % (error.cmd[0], key)
-            new_item = modify_item(item)
-            with open('tmp.bib', 'w') as f:
-                f.write(new_item)
-            substitutions.append((item, new_item))
-        else:
-            print "%s OK"
-            break
+        error = False
+        while True:
+            try:
+                subprocess.check_call(['pdflatex', '-interaction=nonstopmode', 'tmp.tex'])
+                subprocess.check_call(['bibtex', 'tmp'])
+                subprocess.check_call(['pdflatex', '-interaction=nonstopmode', 'tmp.tex'])
+                subprocess.check_call(['pdflatex', '-interaction=nonstopmode', 'tmp.tex'])
+            except subprocess.CalledProcessError as error:
+                print "problem running %s with item %s" % (error.cmd[0], key)
+                help_unicode(item)
+                new_item = modify_item(item)
+                os.remove('tmp.aux')
+                os.remove('tmp.blg')
+                os.remove('tmp.bbl')
+                with open('tmp.bib', 'w') as f:
+                    f.write(new_item)
+                substitutions.append((item, new_item))
+            else:
+                break
+finally:
+    with open('bibtex.bib') as f:
+        biblio = f.read()
+    for old, new in substitutions:
+        biblio = biblio.replace(old, new)
+    with open('bibtex.bib', 'w') as f:
+        f.write(biblio)
+    print "%d fixes done" % len(substitutions)
