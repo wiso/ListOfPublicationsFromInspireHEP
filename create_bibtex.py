2fa0a8c106b6cc9f0f04043d6c27dab8a5f9fe76
Ruggero Turra <giurrero@gmail.com>
2016-02-07 16:23:51 +0100

    add args

--- create_bibtex.py
+++ create_bibtex.py
@@ -1,9 +1,24 @@
 import requests
+import datetime
 from xml.etree import ElementTree
 import io
+import logging
+import argparse
 
+parser = argparse.ArgumentParser(description='Create bibliography from inspirehep',
+                                 formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 epilog='example: create_bibtex.py --query author%3AR.Turra.1+AND+collection%3Apublished')
+parser.add_argument('--baseurl', default="https://inspirehep.net/")
+parser.add_argument('--query', help='query', required=True)
+args = parser.parse_args()
 
-BASEURL = "https://inspirehep.net/"
+logger = logging.getLogger('create bibtex')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)s: %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+BASEURL = args.baseurl
 
 
 def build_query(**kwargs):
@@ -21,12 +36,12 @@ def build_all_queries(nper_step=50, **kwargs):
         kwargs['jrec'] += nper_step
 
 
-args = {'p': 'author%3AR.Turra.1+AND+collection%3Apublished',
-        'of': 'hx', 'em': 'B', 'sf': 'year', 'so': 'd', 'rg': 5, 'tc': 'p', 'jrec': 1}
+inspire_args = {'p': args.query,
+                'of': 'hx', 'em': 'B', 'sf': 'year', 'so': 'd', 'rg': 5, 'tc': 'p', 'jrec': 1}
 
 bibtex = u""
 
-for query in build_all_queries(**args):
+for query in build_all_queries(**inspire_args):
     url = BASEURL + query
     r = requests.get(url)
     if not r.status_code == requests.codes.ok:
@@ -39,8 +54,8 @@ for query in build_all_queries(**args):
 
     bibtex += ''.join(ElementTree.fromstring(r.content).itertext())
     bibtex += r"%% ==============="
-    print '%d items found' % bibtex.count('@')
+    logger.info('%d items found...', bibtex.count('@'))
 
-print '%d items found' % bibtex.count('@')
-f = io.open('bibtex.bib', 'w')
-f.write(bibtex)
+logger.info('%d items found', bibtex.count('@'))
+with io.open('bibtex_%s.bib' % str(datetime.date.today()), 'w') as f:
+    f.write(bibtex)
